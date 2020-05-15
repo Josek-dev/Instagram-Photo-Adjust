3a138006284d61a42a102883fc50832fc95a881b
Josek-dev <xjosekx@gmail.com>
2020-05-16 01:51:45 +0200

    commit

--- dist/transform.dev.js
+++ dist/transform.dev.js
@@ -10,4 +10,17 @@
 // }
 // var newWidth = realWidth;
 // var newHeight = realWidth;
+// window.onload = function() {
+//     path = document.getElementById("scream");
+//     img = new Image();
+//     img.src = path.src;
+//     img.onload = function() {
+//       var canvas = document.createElement('canvas');
+//       var ctx = canvas.getContext('2d');
+//       canvas.width = 360
+//       canvas.height = 360
+//       ctx.drawImage(img, 0, 0, 360, 360);
+//       document.body.appendChild(canvas);
+//     };
+//     }
 "use strict";
